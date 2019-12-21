2ec50bc7af2ae64ea14721893bebb70c90b2dc26
Hannah Harrington <hannahharrington@outlook.com>
2019-12-21 13:08:18 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,10 @@
 class Array
-
   def my_each
-    i = 0
-    while i < self.length
-      yield(self[i])
-      i+=1
-    end 
+    idx = 0
+    while idx < length
+      yield self[idx]
+      idx += 1
+    end
     self
   end
-
 end
