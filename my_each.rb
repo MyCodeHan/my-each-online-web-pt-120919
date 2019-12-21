53b5f277fc72908ae402a249455a4c5f085e0b1d
Hannah Harrington <hannahharrington@outlook.com>
2019-12-21 13:05:28 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1,2,3,4]
+def my_each(collection)
+ my_each = []
+  collection.each do |collection|
+  my_each << "I can count my #{collection}!"
 end
+
+while my_each < 
+end
+end 
